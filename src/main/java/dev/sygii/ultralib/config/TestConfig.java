65dc528189aa89abfe26588101b73849cd6fa7e3
Sygikal <xtin8873@gmail.com>
2025-09-29 19:23:42 -0700

    1.1.4.3 config update

--- src/main/java/dev/sygii/ultralib/config/TestConfig.java
+++ src/main/java/dev/sygii/ultralib/config/TestConfig.java
@@ -3,7 +3,7 @@ package dev.sygii.ultralib.config;
 public class TestConfig extends UltraConfig<TestConfig.Config> {
 
     public TestConfig() {
-        super("test", new String[]{}, Config.class, new Config());
+        super("test", Config.class, new Config());
     }
 
     public static class Config {
